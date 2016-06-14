c44d29ab47dbad5356a62ee9bdd0ec5630498408
ANDY99088 <zhaoce@Mario.local>
2016-06-14 17:56:33 +0800

    adad

---
+++ X_Jiyibi/X_Jiyibi/Classes/HaoYou/Controller/X_AddViewController.h
@@ -0,0 +1,13 @@
+//
+//  X_AddViewController.h
+//  X_Jiyibi
+//
+//  Created by lanou3g on 16/6/14.
+//  Copyright © 2016年 Saina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface X_AddViewController : UIViewController
+
+@end
---
+++ X_Jiyibi/X_Jiyibi/Classes/HaoYou/Controller/X_AddViewController.m
@@ -0,0 +1,37 @@
+//
+//  X_AddViewController.m
+//  X_Jiyibi
+//
+//  Created by lanou3g on 16/6/14.
+//  Copyright © 2016年 Saina. All rights reserved.
+//
+
+#import "X_AddViewController.h"
+
+@interface X_AddViewController ()
+
+@end
+
+@implementation X_AddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
